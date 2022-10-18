7fdc0e234caa7e8045f68d0174b780294efd5a17
Marlon Miller <mmillermatute18@gmail.com>
2022-10-18 10:07:26 -0500

    GSON for my AOT characters

--- app/src/main/java/com/example/mm_json_app_/AotContent.java
+++ app/src/main/java/com/example/mm_json_app_/AotContent.java
@@ -8,11 +8,14 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -24,7 +27,7 @@ public class AotContent {
     public static boolean construct = false;
 
     public void intoGSON(Activity activity) {
-        String url = "https://api.jsonbin.io/v3/b/634eac2d0e6a79321e2c9bf6";
+        String url = activity.getString(R.string.URL);
         RequestQueue queue = Volley.newRequestQueue(activity);
 
         JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
@@ -35,24 +38,27 @@ public class AotContent {
                     JSONArray jsonArray = jsonObjectNode.getJSONArray("AOT"); // Obtains Array named "gameCompanies"
                     ITEMS.clear();
                     ITEM_MAP.clear();
-//                    Gson gson = new Gson();
-//                    String info = jsonArray.toString(); // JSON Array
-//                    Type listType = new TypeToken<ArrayList<Model>>(){}.getType();
-//                    List<Model> games = gson.fromJson(info,listType);
+                    Gson gson = new Gson();
+                    String info = jsonArray.toString(); // JSON Array
+                    Type listType = new TypeToken<ArrayList<Aot>>(){}.getType();
+                    List<Aot> characters = gson.fromJson(info,listType);
 
-
-                    for(int i = 0; i < jsonArray.length(); i++) {
-                        JSONObject object = jsonArray.getJSONObject(i);
-                        String name = object.getString("name");
-                        String titan = object.getString("titan");
-                        int height = object.getInt("height");
-
-
-                        Aot newAot = new Aot(name,titan,height);
-                        ITEMS.add(newAot);
-                        ITEM_MAP.put(name,newAot);
-                        //mTextView.append(model.name + ", " + model.year + ", " + model.recentConsole + "\n");
+                    for(Aot person : characters) {
+                        ITEMS.add(person);
+                        ITEM_MAP.put(person.mName,person);
                     }
+//                    for(int i = 0; i < jsonArray.length(); i++) {
+//                        JSONObject object = jsonArray.getJSONObject(i);
+//                        String name = object.getString("name");
+//                        String titan = object.getString("titan");
+//                        int height = object.getInt("height");
+//
+//
+//                        Aot newAot = new Aot(name,titan,height);
+//                        ITEMS.add(newAot);
+//                        ITEM_MAP.put(name,newAot);
+//                        //mTextView.append(model.name + ", " + model.year + ", " + model.recentConsole + "\n");
+//                    }
                     if(!construct) {
                         activity.recreate();
                     }
